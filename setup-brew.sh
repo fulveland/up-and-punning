eca89d9c19fb45dcdb247f3557d02fd61bbcd2ac
Ivan Reese <ivanreese@gmail.com>
2015-09-24 12:23:14 -0600

    Add nvm

--- setup-brew.sh
+++ setup-brew.sh
@@ -39,6 +39,7 @@ brew_install 'rbenv'
 brew_install 'ruby-build'
 brew_install 'heroku-toolbelt'
 brew_install 'cloc'
+brew_install 'nvm'
 
 
 # Hub gets special treatment because of the --HEAD and my sucking at sh
